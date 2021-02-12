a1e4f18a8c3e44af6dad0a0cd7a7ecbac36e8843
Luis <Luisfrm_@outlook.com>
2021-02-12 14:15:06 -0400

    initial commit

---
+++ arrays_asociativos.php
@@ -0,0 +1,53 @@
+<?php
+$empleado1 = [
+    "nombre" => 'Daniela',
+    "apellido" => 'Denegri',
+    "cedula" => 28137506,
+    "sueldo" => '100$',
+    "departamento" => 'Diseño Gráfico',
+    "lugar_trabajo" => 'DinapixStudio',
+
+];
+echo "<h1> Empleado#1:</h1>";
+echo "Nombre: " . $empleado1['nombre']."<br>";
+echo "Apellido: " . $empleado1['apellido']."<br>";
+echo "Cedula: " . $empleado1['cedula']."<br>";
+echo "Sueldo: " . $empleado1['sueldo']."<br>";
+echo "Departamento: " . $empleado1['departamento']."<br>";
+echo "Lugar de trabajo: " . $empleado1['lugar_trabajo']."<br>";
+
+$empleado2 = [
+    "nombre" => 'Gloriana',
+    "apellido" => 'Jimenez',
+    "cedula" => 28137507,
+    "sueldo" => '150$',
+    "departamento" => 'Informática',
+    "lugar_trabajo" => 'Digitel',
+
+];
+echo "<h1> Empleado#2:</h1>";
+echo "Nombre: " . $empleado2['nombre']."<br>";
+echo "Apellido: " . $empleado2['apellido']."<br>";
+echo "Cedula: " . $empleado2['cedula']."<br>";
+echo "Sueldo: " . $empleado2['sueldo']."<br>";
+echo "Departamento: " . $empleado2['departamento']."<br>";
+echo "Lugar de trabajo: " . $empleado2['lugar_trabajo']."<br>";
+
+$empleado3 = [
+    "nombre" => 'Jesus',
+    "apellido" => 'Pirela',
+    "cedula" => 28137508,
+    "sueldo" => '200$',
+    "departamento" => 'Electrónica',
+    "lugar_trabajo" => 'Mundo Electrónico',
+
+];
+echo "<h1> Empleado#3:</h1>";
+echo "Nombre: " . $empleado3['nombre']."<br>";
+echo "Apellido: " . $empleado3['apellido']."<br>";
+echo "Cedula: " . $empleado3['cedula']."<br>";
+echo "Sueldo: " . $empleado3['sueldo']."<br>";
+echo "Departamento: " . $empleado3['departamento']."<br>";
+echo "Lugar de trabajo: " . $empleado3['lugar_trabajo']."<br>";
+
+?>
